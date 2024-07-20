2a39e1dfbd1511701bc4fd263faf5b6982a69a03
idrazovic <idrazovic@gmail.com>
2024-07-20 22:57:17 +0200

    users list and tasks components addde

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,27 @@ import { RouterOutlet } from '@angular/router';
 
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from './user/user.component';
+import { DUMMY_USERS } from './dummy-users';
+import { TasksComponent } from './tasks/tasks.component';
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [RouterOutlet, HeaderComponent, UserComponent],
+    imports: [
+        RouterOutlet,
+        HeaderComponent,
+        UserComponent,
+        TasksComponent
+    ],
     templateUrl: './app.component.html',
     styleUrl: './app.component.scss'
 })
-export class AppComponent { }
+export class AppComponent {
+    users = DUMMY_USERS;
+    selectedUser = this.users[0];
+
+    onSelectUser(id: string) {
+        const user = this.users.find(user => user.id === id);
+        this.selectedUser = user ?? this.users[0];
+    }
+}
